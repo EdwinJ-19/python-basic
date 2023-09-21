551a7cf7d50f3cf5d33a3594678b7b6097ca71dd
EdwinJ-19 <edwinjames2001@gmail.com>
2023-09-21 22:04:42 +0530

    python-1

---
+++ try.py
@@ -0,0 +1,8 @@
+name="Edwin"    #this is just basic u are seeing (single line comment)
+'''name="Tuchiya"'''    #multiline comment basic u can say :)
+print(name)     
+
+a = 10
+b = 20
+c = a+b
+print(c)
